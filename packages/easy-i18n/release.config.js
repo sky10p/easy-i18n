a1d56468920cad0e75e00c07db9fe93bebb5c63c
Pablo Guijarro <sky10pg@gmail.com>
2023-03-14 22:34:57 +0100

    feat: added release step

---
+++ packages/easy-i18n/release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    branches: ['master'], // nombre de la rama de git donde se hace la publicación
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/npm',
+      '@semantic-release/changelog',
+      '@semantic-release/git',
+    ],
+  };
